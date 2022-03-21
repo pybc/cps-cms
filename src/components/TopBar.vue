b0592331d32caa2d52e2dcfcb9379276bc11c501
pybc <piyabute.c@gmail.com>
2022-03-21 15:47:44 +0700

    Create TopBar.vue

---
+++ src/components/TopBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="top-bar" class="ml-64 py-5 bg-white">
+    <nav class="flex flex-row justify-end mr-5 text-slate-800 items-center">
+      <div class="mx-2 user">
+        <img src="@/assets/user-mockup.jpg" alt="" />
+      </div>
+      <div>DisplayName</div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#top-bar {
+  border-bottom: 1px solid gainsboro;
+}
+.user {
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+}
+</style>
