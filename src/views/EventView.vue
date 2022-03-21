492f427b22cd9f50c961b325bc2c9ff3eeca8e83
pybc <piyabute.c@gmail.com>
2022-03-21 22:04:45 +0700

    Init Eventview

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="event">
-    <h1>This is an event page</h1>
+    <div class="flex justify-between px-5">
+      <div class="text-3xl font-bold py-8">Event</div>
+
+      <div class="flex items-center">
+        <SearchBar @searchInput="receiveInputValue" class="mx-1" />
+        <DropDown
+          class="mx-1"
+          value="Select your filter"
+          :option="[
+            `Last
+        12 Months`,
+            `Last 13 Months`,
+            `Last 14 Months`,
+          ]"
+        />
+        <div
+          class="flex pr-4 pl-1 rounded items-center bg-primary cursor-pointer"
+        >
+          <PlusIcon class="text-slate-100 opacity-70 mx-1" />
+          <button class="text-white text-sm py-2 drop-shadow-sm">
+            Add Event
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 // @ is an alias to /src
-
+import SearchBar from "../components/SearchBar.vue";
+import DropDown from "../components/DropDown.vue";
+import { PlusIcon } from "@vue-hero-icons/outline";
 export default {
   name: "EventView",
-  components: {},
+  components: { SearchBar, DropDown, PlusIcon },
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+  methods: {
+    receiveInputValue(searchValue) {
+      this.searchValue = searchValue;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped></style>
