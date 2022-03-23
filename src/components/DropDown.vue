308850bce31d5188b062e29d674d0f602e4c7621
Thapanut Saripat <thapanutsaripat@gmail.com>
2022-03-24 01:54:33 +0700

    Add transition

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -14,25 +14,35 @@
     </button>
 
     <!-- DISPLAY SELECT MENU -->
-    <div v-if="isOpen && option" class="absolute z-50 hover:z-100">
-      <div
-        class="border border-slate-300 mt-1 outline-none rounded w-44 truncate text-sm bg-white"
-      >
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+      v-if="isOpen && option"
+    >
+      <div class="absolute z-50 hover:z-100">
         <div
-          v-for="(item, index) in option"
-          :key="index"
-          class="hover:bg-slate-100 pl-7 pr-2 py-2 cursor-pointer text-slate-500 capitalize"
-          @click="
-            () => {
-              defaultValue = item;
-              $emit('dropDownValue', item);
-            }
-          "
+          class="border border-slate-300 mt-1 outline-none rounded w-44 truncate text-sm bg-white"
         >
-          {{ item }}
+          <div
+            v-for="(item, index) in option"
+            :key="index"
+            class="hover:bg-slate-100 pl-7 pr-2 py-2 cursor-pointer text-slate-500 capitalize"
+            @click="
+              () => {
+                defaultValue = item;
+                $emit('dropDownValue', item);
+              }
+            "
+          >
+            {{ item }}
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
