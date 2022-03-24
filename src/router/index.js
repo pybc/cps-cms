0deff6cdb64dca5651d2b4fc0406bc9bf57ee919
pybc <piyabute.c@gmail.com>
2022-03-24 22:29:58 +0700

    Change router path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import EventView from "../views/EventView.vue";
+import EventView from "../views/Event/EventView.vue";
 import RewardView from "../views/RewardView.vue";
 import AchievementView from "../views/AchievementView.vue";
 
