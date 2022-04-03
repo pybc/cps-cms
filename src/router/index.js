e240b6655b638edf85d83a894abc78e96e897956
Piyabute Chairiboon <piyabute.c@gmail.com>
2022-04-03 22:58:53 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import EventView from "../views/Event/EventView.vue";
+import EventMember from "../views/Event/EventMember.vue";
+import EventMessage from "../views/Event/EventMessage.vue";
 import EventCreate from "../views/Event/EventCreate.vue";
 import RewardView from "../views/Reward/RewardView.vue";
 import RewardCreate from "../views/Reward/RewardCreate.vue";
@@ -19,6 +21,16 @@ const routes = [
     path: "/event",
     name: "event",
     component: EventView,
+  },
+  {
+    path: "/event/member/:eventId",
+    name: "eventMember",
+    component: EventMember,
+  },
+  {
+    path: "/event/message/:eventId",
+    name: "eventMessage",
+    component: EventMessage,
   },
   {
     path: "/event/create",
